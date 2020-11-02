6c7273f609b899f1000cc81bd68e5debdebcbd64
VSRUJANA <srujana.valavala@gmail.com>
2020-11-02 15:25:59 +0530

    [srujana] Refactor. employee_payroll table created in the database

--- EmployeePayroll.sql
+++ EmployeePayroll.sql
@@ -4,3 +4,11 @@ create database Payroll_Service;
 use Payroll_Service;
 select DB_NAME();
 
+-- Create table in the database
+create table employee_payroll
+(
+id int identity(1,1),
+name varchar(25) not null,
+salary money not null,
+start date not null
+);
