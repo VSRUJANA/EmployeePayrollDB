138d1376eff414d4beb380dfd96f437a345ece3c
VSRUJANA <srujana.valavala@gmail.com>
2020-11-02 15:34:33 +0530

    [srujana] Refactor. Data inserted into employee_payroll table

--- EmployeePayroll.sql
+++ EmployeePayroll.sql
@@ -12,3 +12,11 @@ name varchar(25) not null,
 salary money not null,
 start date not null
 );
+
+-- Display table details
+select * from INFORMATION_SCHEMA.COLUMNS where TABLE_NAME = 'employee_payroll';
+-- Inserting data in table
+insert into employee_payroll values
+('Bill',100000.00,'2018-10-03'),
+('Teresa',200000.00,'2019-08-28'),
+('Charlie',300000.00,'2020-04-05');
